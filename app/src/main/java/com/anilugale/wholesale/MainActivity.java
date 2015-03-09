43c72d746fe463c501df513a24e01392fdbcf6fe
anil1989 <anilugale1989@gmail.com>
2015-03-09 15:09:43 +0530

    adding new activity

--- app/src/main/java/com/anilugale/wholesale/MainActivity.java
+++ app/src/main/java/com/anilugale/wholesale/MainActivity.java
@@ -1,7 +1,10 @@
 package com.anilugale.wholesale;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.anilugale.wholesale.adapter.CategoryAdapter;
@@ -19,21 +22,35 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-
     }
 
     private void init() {
-
         listView=(ListView) findViewById(R.id.main_list);
         List<String> listData=new ArrayList<>();
         for(int i=0;i<10;i++)
         {
             listData.add("Category :"+i);
         }
-
         adapter=new CategoryAdapter(this,listData);
         listView.setAdapter(adapter);
+    }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this,Vendor.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
