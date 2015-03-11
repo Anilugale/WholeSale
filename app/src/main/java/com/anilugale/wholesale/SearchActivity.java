d129b9a31ca0306d040c3296a75b769750ca6b94
anil1989 <anilugale1989@gmail.com>
2015-03-11 16:49:41 +0530

    Added map activity and user location marker

--- app/src/main/java/com/anilugale/wholesale/SearchActivity.java
+++ app/src/main/java/com/anilugale/wholesale/SearchActivity.java
@@ -3,9 +3,11 @@ package com.anilugale.wholesale;
 import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,7 +16,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class SearchActivity extends FragmentActivity {
+public class SearchActivity extends FragmentActivity implements LocationListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
@@ -22,6 +24,10 @@ public class SearchActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 35000, 10, this);
+
         setUpMapIfNeeded();
     }
 
@@ -55,27 +61,50 @@ public class SearchActivity extends FragmentActivity {
         // set map type
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-        // Get latitude of the current location
-        double latitude = myLocation.getLatitude();
+      if(myLocation!=null) {
+          double latitude = myLocation.getLatitude();
+          double longitude = myLocation.getLongitude();
+          LatLng latLng = new LatLng(latitude, longitude);
+          mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+          mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
+          LatLng latLng1 = new LatLng(latitude, longitude);
+          mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng1, 16.0f));
+          mMap.addMarker(new MarkerOptions().position(latLng1).title("You are here!").snippet("Consider yourself located").icon(BitmapDescriptorFactory.fromResource(R.drawable.user_marker)));
+      }
+        else
+      {
 
-        // Get longitude of the current location
-        double longitude = myLocation.getLongitude();
+          LatLng latLng = new LatLng( 21.1289956,82.7792201);
+          mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+          mMap.animateCamera(CameraUpdateFactory.zoomTo(3));
 
-        // Create a LatLng object for the current location
-        LatLng latLng = new LatLng(latitude, longitude);
-
-        // Show the current location in Google Map
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-
-        // Zoom in the Google Map
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
-
-        LatLng latLng1=new LatLng(latitude, longitude);
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng1, 16.0f));
-        mMap.addMarker(new MarkerOptions().position(latLng1).title("You are here!").snippet("Consider yourself located").icon(BitmapDescriptorFactory.fromResource(R.drawable.user_marker)));
+          mMap.clear();
+          Toast.makeText(this,"your location setting is off turn on to get better experience.",Toast.LENGTH_SHORT).show();
+      }
     }
 
 
+    @Override
+    public void onLocationChanged(Location location) {
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Toast.makeText(this,"Location provider on.",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Toast.makeText(this,"Location provider off.",Toast.LENGTH_SHORT).show();
+    }
 
 
 }
+
